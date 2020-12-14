ebe54a7e594e50795895b542714f680ba0e7b2da
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2020-12-14 10:09:32 -0300

    Atualizando site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
-
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateY(100%)', opacity: 0}),
+          animate('400ms', style({transform: 'translateY(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateY(0)', opacity: 1}),
+          animate('400ms', style({transform: 'translateY(100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
@@ -14,42 +28,62 @@ export class AppComponent implements OnInit {
     {
       'titulo' : 'FGA N-Rainhas',
       'desc'   : 'Implementação de algoritmo genético para resolver o problema das N-rainhas.',
-      'link'   : 'https://github.com/leonamtv/FGA-N-Rainhas'
+      'link'   : 'https://github.com/leonamtv/FGA-N-Rainhas',
+      'tags'   : [ 'python', 'IA', 'AG' ]
     },
     {
       'titulo' : 'Frame Analyser',
       'desc'   : 'Um script em python3 para analisar um vídeo ou um filme e gerar um html com suas cores predominantes.',
-      'link'   : 'https://github.com/leonamtv/frame-analyser'
+      'link'   : 'https://github.com/leonamtv/frame-analyser',
+      'tags'   : [ 'python' ]
     },
     {
       'titulo' : 'Nerdcast Downloader',
       'desc'   : 'Pequeno webcrawler que faz a busca do último nerdcast e faz o download.',
-      'link'   : 'https://github.com/leonamtv/nerdcast-downloader'
+      'link'   : 'https://github.com/leonamtv/nerdcast-downloader',
+      'tags'   : [ 'python' ]
     },
     {
       'titulo' : 'Lyrics Analyser',
       'desc'   : 'Pequeno webcrawler para fazer o download de letras de artistas e verificar a frequência de palavras',
-      'link'   : 'https://github.com/leonamtv/lyrics-analyser'
+      'link'   : 'https://github.com/leonamtv/lyrics-analyser',
+      'tags'   : [ 'python' ]
     },
     {
       'titulo' : 'Criador de projetos',
       'desc'   : 'Software para automatizar a criação de projetos.',
-      'link'   : 'https://github.com/leonamtv/create-base-project'
+      'link'   : 'https://github.com/leonamtv/create-base-project',
+      'tags'   : [ 'python' ]
     },
     {
       'titulo' : 'Cubo de led 8x8x8',
       'desc'   : 'Frontend do projeto apresentado pelo meu grupo na META do CEFET-MG 2019.',
-      'link'   : 'https://github.com/leonamtv/Cubo-de-led-8x8x8'
+      'link'   : 'https://github.com/leonamtv/Cubo-de-led-8x8x8',
+      'tags'   : [ 'angular', 'typescript' ]
     },
     {
       'titulo' : 'Space X',
       'desc'   : 'Aplicativo simples para consumir alguns recursos da api graphql pública da SpaceX.',
-      'link'   : 'https://github.com/leonamtv/spacex'
+      'link'   : 'https://github.com/leonamtv/spacex',
+      'tags'   : [ 'flutter', 'dart' ]
     },
     {
       'titulo' : 'Wgmon',
       'desc'   : 'Aplicativo para monitorar peso usando como base de dados uma simples planilha do Google Sheets. Regressão linear é utilizada para fazer predição.',
-      'link'   : 'https://github.com/leonamtv/wgmon'
+      'link'   : 'https://github.com/leonamtv/wgmon',
+      'tags'   : [ 'flutter', 'dart' ]
+    },
+    {
+      'titulo' : 'Clexer',
+      'desc'   : 'Analisador léxico para a linguagem ANSI-C. Trabalho da disciplina de compiladores.',
+      'link'   : 'https://github.com/leonamtv/clexer',
+      'tags'   : [ 'python' ]
+    },
+    {
+      'titulo' : 'HTMLParser',
+      'desc'   : 'Parser de HTML para a disciplina de compiladores.',
+      'link'   : 'https://github.com/leonamtv/html-parser',
+      'tags'   : [ 'python' ]
     },
   ];
 
@@ -57,8 +91,8 @@ export class AppComponent implements OnInit {
     'interesses' : "Meus interesses são voltados para as áreas de desenvolvimento web, desenvolvimento mobile, análise de dados e machine learning.",
     'formacao' : [
       "Tenho curso de aprendizagem e manutenção em redes de microcomputadores no SENAI",
-      "Sou técnico em informática pelo CEFET-MG campus Timóteo (aguardando colação de grau)",
-      "Sou aluno do 7º período de engenharia de computação pelo CEFET-MG campus Timóteo"
+      "Sou técnico em informática pelo CEFET-MG campus Timóteo",
+      "Sou aluno do 8º período de engenharia de computação pelo CEFET-MG campus Timóteo"
     ],
     'habilidades-tec' : {
       'title' : 'Habilidades técnicas',
@@ -113,6 +147,9 @@ export class AppComponent implements OnInit {
       ]
     }
   };
+
+  visible : boolean;
+
   constructor(@Inject(DOCUMENT) document) {}
   
   ngOnInit(): void {
@@ -126,4 +163,17 @@ export class AppComponent implements OnInit {
   scrollToElement($element): void {
     $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
+
+  gotToTop () {
+    this.scrollToElement(window.document.getElementById('home'));
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll ( event ) {
+    let homeTop       = window.document.getElementById('home').getBoundingClientRect().top;
+    let screenHeight  = window.innerHeight;
+    this.visible      = (( Math.abs(homeTop) + 300 )< screenHeight ) ? false : true;
+  }
+
+
 }
